fdc5196c486394d8bd9c17aef61bc9e8352748e2
Fergoth <fergothew@gmail.com>
2019-04-25 12:33:15 +0300

    Init_commit

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,47 @@
+import requests
+import datetime
+import pprint
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_week_ago():
+    week = datetime.timedelta(days=7)
+    week_ago = datetime.datetime.today() - week
+    return week_ago.strftime('%Y-%m-%d')
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(top_size=20):
+    date = get_week_ago()
+    params = {'q': 'created:>{}'.format(date),
+              'sort': 'stars'}
+    url = "https://api.github.com/search/repositories"
+    r = requests.get(url, params=params)
+    return r.json()['items'][:top_size]
+
+
+def get_open_issues_amount(repositories):
+    for repo in repositories:
+        url = repo['issues_url'].strip('{/number}')
+        r = requests.get(url)
+        issue_info = r.json()
+        repo['open_issues_info'] = issue_info
+    # https: // api.github.com / repos / username / reponame / issues
+
+
+def print_repo_info(repo):
+    print('Name : {}'.format(repo['name']))
+    print('Description : {}'.format(repo['description']))
+    print('Stars : {}'.format(repo['stargazers_count']))
+    print('Issues count : {}'.format(repo['open_issues']))
+    for issue in repo['open_issues_info']:
+        print('    Issue url: {}'.format(issue['html_url']))
+
+
+def print_repositories_info(repositories):
+    print('Репозитории с наибольшим количеством звёзд за неделю')
+    for repo in repositories:
+        print_repo_info(repo)
 
 if __name__ == '__main__':
-    pass
+    trending_repositories = get_trending_repositories()
+    get_open_issues_amount(trending_repositories)
+    print_repositories_info(trending_repositories)
